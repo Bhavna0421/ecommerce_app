7df5f095622490645410a4a86c8caf37942c31e4
bhavna0421 <bhavnasharma0421@gmail.com>
2023-07-24 19:50:46 +0530

    fix : page redirecting

--- src/components/wishlistItem/wishlistItem.tsx
+++ src/components/wishlistItem/wishlistItem.tsx
@@ -14,64 +14,65 @@ export default function WishlistItem({ product }: Props) {
   console.log("productquantity", product.id);
   return (
     <div style={{ display: "flex", flexWrap: "wrap", marginBottom: "1rem" }}>
-      <Link
-      style={{textDecoration:"none"}}
-        href={{
-          pathname: "/[id]",
-          query: { id: product.id },
+      <Paper
+        key={product.title}
+        sx={{
+          display: "flex",
+          padding: "7px",
+          maxWidth: 400,
         }}
       >
-        <Paper
-          key={product.title}
-          sx={{
-            display: "flex",
-            padding: "7px",
-            maxWidth: 400,
-          }}
-        >
-          <ButtonBase sx={{ width: 128, height: 128 }}>
-            <img
-              style={{
-                margin: "auto",
-                display: "block",
-                maxWidth: "100%",
-                maxHeight: "100%",
-              }}
-              alt="complex"
-              src={product.thumbnail}
-            />
-          </ButtonBase>
+        <ButtonBase sx={{ width: 128, height: 128 }}>
+          <img
+            style={{
+              margin: "auto",
+              display: "block",
+              maxWidth: "100%",
+              maxHeight: "100%",
+            }}
+            alt="complex"
+            src={product.thumbnail}
+          />
+        </ButtonBase>
 
-          <Grid container spacing={2} sx={{ marginLeft: "1rem", flexGrow: 1 }}>
-            <Grid item xs={12} sm={12}>
-              <Typography variant="subtitle1" component="div">
-                {product.title}
+        <Grid container spacing={2} sx={{ marginLeft: "1rem", flexGrow: 1 }}>
+          <Grid item xs={12} sm={12}>
+            <Typography variant="subtitle1" component="div">
+              {product.title}
+            </Typography>
+            <Typography variant="subtitle1" component="div">
+              ${product.price}
+            </Typography>
+
+            <Typography variant="body2" gutterBottom>
+              <Link
+                href={{
+                  pathname: "/[id]",
+                  query: { id: product.id },
+                }}
+                style={{ textDecoration: "none", color: "grey" }}
+              >
+                {product.description}
+              </Link>
+            </Typography>
+            <div style={{ marginLeft: "6.5rem" }}>
+              <Typography
+                style={{
+                  cursor: "pointer",
+                  fontSize: "11.5px",
+                  color: "red",
+                }}
+                onClick={() => {
+                  cogoToast.info("Removed from the wishlist");
+                  return removeFromCart(product);
+                }}
+              >
+                Remove from wishlist
               </Typography>
-              <Typography variant="subtitle1" component="div">
-                ${product.price}
-              </Typography>
-              <Typography variant="body2" gutterBottom>
-                <p>{product.description}</p>
-              </Typography>
-              <div style={{ marginLeft: "6.5rem" }}>
-                <Typography
-                  style={{
-                    cursor: "pointer",
-                    fontSize: "11.5px",
-                    color: "red",
-                  }}
-                  onClick={() => {
-                    cogoToast.info("Removed from the wishlist");
-                    return removeFromCart(product);
-                  }}
-                >
-                  Remove from wishlist
-                </Typography>
-              </div>
-            </Grid>
+            </div>
           </Grid>
-        </Paper>
-      </Link>
+        </Grid>
+      </Paper>
     </div>
   );
 }
